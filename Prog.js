ff89413760eae8e5f77060a22c61339a5da9565b
Lubomir1976 <lLubomir197@rambler.ru>
2020-12-28 20:25:14 +0300

    Function

--- Prog.js
+++ Prog.js
@@ -6,3 +6,17 @@ while (num < 55) {
     console.log(num)
     num++
 }
+function showFirstMessage(){
+    console.log('Всем приает!!!');
+}
+showFirstMessage()
+
+const str = 'test';
+const arr = [1, 2, 3];
+
+console.log(str.length)
+console.log(arr.length)
+
+//Округления
+const num = 12.2;
+console.log(Math.round(num));
